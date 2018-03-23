b385393754ae30ceaed528042d50e6760c37003c
chexingyou <hingsir1024@gmail.com>
2018-03-23 15:37:07 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+var fs = require('fs')
+var path = require('path')
+
+function encode(filename, data){
+  var extname = path.extname(filename).substr(1);
+  extname = extname || 'png';
+
+  if (extname === 'svg') {
+    extname = "svg+xml"
+  }
+
+  return 'data:image/' + extname + ';base64,' + data.toString('base64');
+}
+
+module.exports = function (filename, callback) {
+  if (!callback) callback = util.noop;
+
+  fs.readFile(filename, function(err, data) {
+    if (err) return callback(err);
+    callback(null, encode(filename, data));
+  });
+}
